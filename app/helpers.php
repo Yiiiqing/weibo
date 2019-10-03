f4d8c427471dc03e99a5d03a95e97416302613bd
yiqing <y.zhang@live.com>
2019-10-03 16:19:24 +0000

    Update database configuration

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 function get_db_config()
 {
     if (getenv('IS_IN_HEROKU')) {
@@ -14,32 +13,10 @@ function get_db_config()
     } else {
         return $db_config = [
             'connection' => env('DB_CONNECTION', 'mysql'),
-=======
-
-function get_db_config()
-{
-    if(getenv('IS_IN_HEROKU')){
-        $url = perse_url(getenv("DATABASE_URL"));
-
-        return $db_config = [
-            'connection' => 'pgsql',
-            'host' => url['host'],
-            'database' => substr($url["path"], 1),
-            'username' => $url["user"],
-            'password' => $url["pass"],
-        ];
-    }else{
-        return $db_config = [
-            'connection' => env('DB_CONNECTION','mysql'),
->>>>>>> 5866415510aff0cf208ce83d7e5f37261f17c762
             'host' => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
-<<<<<<< HEAD
-=======
-
->>>>>>> 5866415510aff0cf208ce83d7e5f37261f17c762
         ];
     }
 }
--- resources/views/shared/_messages.blade.php
+++ resources/views/shared/_messages.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @foreach (['danger', 'warning', 'success', 'info'] as $msg)
   @if(session()->has($msg))
     <div class="flash-message">
@@ -7,14 +6,4 @@
       </p>
     </div>
   @endif
-=======
-@foreach(['danger','warning','success','info'] as $msg)
-    @if(session()->has($msg))
-        <div class="flash-msg">
-          <p class="alert alert-{{ $msg }}">
-            {{session()->get($msg)}}
-          </p>
-        </div>
-    @endif
->>>>>>> 5866415510aff0cf208ce83d7e5f37261f17c762
 @endforeach
