6e264bc78ad969534bab00faa9b3968db5e08356
yiqing <y.zhang@live.com>
2019-10-07 16:50:33 +0000

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+
 class SessionsController extends Controller
 {
     public function create()
@@ -17,7 +18,7 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if (Auth::attempt($credentials)){
+        if (Auth::attempt($credentials, $request->has('remember'))){
             //登录成功
             session()->flash('success','欢迎回来！');
             return redirect()->route('users.show',[Auth::user()]);
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -8,20 +8,33 @@
     </div>
     <div class="card-body">
       @include('shared._errors')
+
       <form method="POST" action="{{ route('login') }}">
           {{ csrf_field() }}
+
           <div class="form-group">
             <label for="email">邮箱：</label>
             <input type="text" name="email" class="form-control" value="{{ old('email') }}">
           </div>
+
           <div class="form-group">
             <label for="password">密码：</label>
             <input type="password" name="password" class="form-control" value="{{ old('password') }}">
           </div>
+
+          <div class="form-group">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
+              <label class="form-check-lable" for="exampleCheck1">记住我</label>
+            </div>
+          </div>
           <button type="submit" class="btn btn-primary">登录</button>
       </form>
+
       <hr>
+
       <p>还没账号？<a href="{{ route('signup') }}">现在注册！</a></p>
+
     </div>
   </div>
 </div>
