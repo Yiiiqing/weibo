e66ad00bfcfd983cfd3366c4befd11a6f0a3994c
yiqing <y.zhang@live.com>
2019-10-17 19:36:14 +0000

    修改主页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -22,8 +22,12 @@
     </div>
   @else
     <div class="jumbotron">
-      <h1>Hello Laravel</h1>
-      <p class="lead">你现在所看到的是<a href="https://github.com/Yiiiqing/weibo">Laravel weibo project</a>的示例项目主页</p>
+      <h1>Hello！(￣▽￣)~*</h1>
+      <p class="lead">你现在所看到的是 🐽张一清 的 微博💬 网站！</p>
+      <h4>你怎么发现这个角落的？是不是我告诉你的？如果是的话，那就说明：<hr/></h4><h4>我爱你❤️</h4><p>喂！屏幕面前的那位！说你呢</p>
+      <hr/>
+      <h5>什么？不是？那你还待着干嘛！</h5>
+      <p>项目Git主页<a href="https://github.com/Yiiiqing/weibo">Laravel weibo project</a></p>
       <p>一切，将从这里开始</p>
       <p>
         <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">现在注册</a>
